0f489199443075fdeceb7c5cf2f3be44b4bc8dff
stef2805 <122122338+stef2805@users.noreply.github.com>
2023-05-31 15:16:42 +0300

    final

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from functional import seq
 
+def isPrim(numar):
+    ind = 2
+    while ind<=numar/2:
+        if(numar%ind==0):
+            return False
+        ind = ind+1
+    return True
+
 if __name__ == '__main__':
-    text = "acesta este textul care se va reuni dupa prima litera din fiecare cuvant din el"
-    listaMea = text.split(" ")
-    print(listaMea)
-    listaMea2 = seq(listaMea).map(lambda element: [element[0], element])
+    listaMea = list([1,6,2,98,12,53,23,87,4])
+    listaMea2 = seq(listaMea).filter(lambda element: isPrim(element) and element%2==1 and element<50)
     print(listaMea2)
-    listaMea3 = listaMea2.group_by(lambda element: element[0]).map(lambda element: [element[0],element[1]])
-    print(listaMea3)
+
 
