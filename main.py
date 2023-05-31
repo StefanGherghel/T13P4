d90df74e5c007d7d509836b056c4a049e5c36695
stef2805 <122122338+stef2805@users.noreply.github.com>
2023-05-31 15:08:50 +0300

    final

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from functional import seq
 
 if __name__ == '__main__':
-    listaMea = list([1, 21, 75, 39, 7, 2, 35, 3, 31, 7, 8])
-
-    listaMea2 = seq(listaMea).filter(lambda elem: elem>5)           #a)
+    text = "acesta este textul care se va reuni dupa prima litera din fiecare cuvant din el"
+    listaMea = text.split(" ")
+    print(listaMea)
+    listaMea2 = seq(listaMea).map(lambda element: [element[0], element])
     print(listaMea2)
-
-    listaMea3 = seq(listaMea2).grouped(2)                            #b)
+    listaMea3 = listaMea2.group_by(lambda element: element[0]).map(lambda element: [element[0],element[1]])
     print(listaMea3)
-
-    listaMea4 = seq(listaMea3).map(lambda elem: elem[0]*elem[1])     #c)
-    print(listaMea4)
-
-
-    listaMea5 = seq(listaMea4).sum()
-    print(listaMea5)
 
